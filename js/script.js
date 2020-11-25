7d9383216777bdee9b67765494fff05789c3b6c8
Max Ventura <maxbim3.14@gmail.com>
2020-11-25 09:29:14 -0500

    Segunda Versión Jardín
    El diseño ya va quedando, solo falta ordenar las imagenes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function enviar() {
+
+  var username = [document.getElementById("nombre").value];
+  var usersug = document.getElementById("sugerencia").value;
+  var addComentario=document.getElementById("addComentario");
+  //addComentario.innerHTML="@" + username + ": " + usersug;
+  addComentario.innerHTML="@Max: Ajam, te engañé, por el momento no está disponible la sección de comentarios :)";
+}
